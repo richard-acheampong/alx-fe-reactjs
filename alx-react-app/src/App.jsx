1eab3a05e10c0c9b1f51370fc426c8eaf5210d24
richard-acheampong <richardkaacheampong@gmail.com>
2025-11-04 13:29:42 +0000

    Created a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header';
-import Footer from './Footer'
-import MainContent from './MainContent';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -24,6 +25,7 @@ function App() {
       <h1>Vite + React</h1>
       <WelcomeMessage />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
