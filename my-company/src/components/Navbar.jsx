e6ad922a427170376a28746ad22f0c2e22757fe8
richard-acheampong <richardkaacheampong@gmail.com>
2025-11-09 18:57:41 +0000

    Added justifyContent style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+    <nav style={{ backgroundColor: '#333', padding: '10px', display: 'flex', justifyContent: 'center'}}>
       <Link to="/" style={{ color: '#fff', margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ color: '#fff', margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ color: '#fff', margin: '0 10px' }}>Services</Link>
