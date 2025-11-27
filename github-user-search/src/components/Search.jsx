d134a87cabdc68ddd5b8a2192aaf0677226220aa
richard-acheampong <richardkaacheampong@gmail.com>
2025-11-27 13:24:39 +0000

    removed ['] from error statement

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -21,7 +21,7 @@ export default function SearchUser() {
       setUser(data);
     } catch (err) {
       console.error(err);
-      setError('Looks like we can’t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false);
     }
