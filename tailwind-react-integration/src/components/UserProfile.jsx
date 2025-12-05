a655502ccbce095f9ddaecd9fcf620061f5735ad
richard-acheampong <richardkaacheampong@gmail.com>
2025-12-05 19:31:03 +0000

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,10 +2,12 @@
 // src/components/UserProfile.jsx
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 p-8 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img className = "rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-xl sm:text-lg md:text-xl text-blue-800 my-4">
+        John Doe
+      </h1>
+      <p className="text-base sm:text-sm md:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
