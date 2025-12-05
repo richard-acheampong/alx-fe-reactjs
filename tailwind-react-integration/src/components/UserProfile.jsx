069cbe13add3e7ddfdefb76c8618046423ecf77c
richard-acheampong <richardkaacheampong@gmail.com>
2025-12-05 19:34:19 +0000

    Responsive design implementation on image

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-      <img className = "rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
+      <img className = "rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" src="https://via.placeholder.com/150" alt="User" />
       <h1 className="text-xl sm:text-lg md:text-xl text-blue-800 my-4">
         John Doe
       </h1>
