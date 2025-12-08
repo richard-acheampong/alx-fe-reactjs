150c6df0d05c3c0dbdbf1d6c885576da62ba2c08
richard-acheampong <richardkaacheampong@gmail.com>
2025-12-08 17:26:17 +0000

    Set Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React/Vite paths
+    "./public/index.html"
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
