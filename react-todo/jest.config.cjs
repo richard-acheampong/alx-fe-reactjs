747d98d754ad5228b7e06de5ebdbb2ea3fc83d76
richard-acheampong <richardkaacheampong@gmail.com>
2025-12-16 14:46:33 +0000

    created test.config.cjs

---
+++ react-todo/jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx)$': ['@swc/jest'],
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+};
