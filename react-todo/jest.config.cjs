ec3bbbe246be514fcc19184288ec4bdb192eaf46
richard-acheampong <richardkaacheampong@gmail.com>
2025-12-16 14:42:45 +0000

    configured Jest with @swc/jest and add .swcrc for JSX support

--- react-todo/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  transform: {
-    '^.+\\.(js|jsx)$': ['@swc/jest'],
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json'],
-};
